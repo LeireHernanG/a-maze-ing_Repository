26c4bdda7d47af5eadf41c1be00ff9c3a63def09
pmieres- <pmieres-@student.42madrid.com>
2026-05-11 16:24:41 +0200

    version more clean and with a minimum of cells to cut in no perfect option

--- maze.py
+++ maze.py
@@ -1,10 +1,11 @@
 import numpy as np
 import random
-from pydantic import (BaseModel, Field, ValidationError, model_validator)
+from pydantic import (BaseModel, Field, ValidationError, model_validator, PrivateAttr)
 from typing import Any
 from typing_extensions import Self
 from collections import deque
 import math
+from numpy.typing import NDArray
 
 
 # north = 3
@@ -35,6 +36,29 @@ class Configuration(BaseModel):
     output_file: str
     perfect: bool
     seed: int = 42
+    _visited = PrivateAttr()
+
+    def model_post_init(self, __context) -> None:
+        self._visited = self.write_42()
+
+    def write_42(self) -> NDArray[np.int_]:
+        visited = np.full((self.height, self.width), 0)
+        if self.height < 8 or self.width < 10:
+            return visited
+        pattern = [
+            '1   111',
+            '1     1',
+            '111 111',
+            '  1 1  ',
+            '  1 111'
+        ]
+        x0 = self.width // 2 - len(pattern[0]) // 2
+        y0 = self.height // 2 - len(pattern) // 2
+        for y, row in enumerate(pattern):
+            for x, value in enumerate(row):
+                if value != ' ':
+                    visited[y0 + y, x0 + x] = 1
+        return visited
 
     @model_validator(mode='after')
     def check_rules(self) -> Self:
@@ -61,10 +85,21 @@ class Configuration(BaseModel):
             raise ValueError('The exit y position is higher than the height')
         if self.exit == self.entry:
             raise ValueError('The exit and the entry must bu different')
+        if self._visited[self.entry[1], self.entry[0]]:
+            raise ValueError('The entry position is in the 42 pattern')
+        if self._visited[self.exit[1], self.exit[0]]:
+            raise ValueError('The exit position is in the 42 pattern')
         return self
 
 
 class MazeGenerator():
+
+    _directions = [
+                    (0, -1, 0),   # north
+                    (1, 0, 1),   # east
+                    (0, 1, 2),   # south
+                    (-1, 0, 3)  # west           
+                ]
 
     def __init__(self, maze_data: Configuration):
         self.width = maze_data.width
@@ -73,86 +108,55 @@ class MazeGenerator():
         self.exit = maze_data.exit
         self.perfect = maze_data.perfect
         self.maze = np.full((maze_data.height, maze_data.width), 15)
-        self.visited = np.full((maze_data.height, maze_data.width), 0)
-        self.write_42()
+        self.visited = maze_data._visited
 
-    def get_neighbors(self, col: int, row: int) -> list:
+    def get_available_walls(self, col: int, row: int) -> list:
         walls = []
-        dirs = [
-                    (-1, 0, 3, 1),  # west
-                    (0, 1, 2, 0),   # south
-                    (1, 0, 1, 3),   # east
-                    (0, -1, 0, 2)   # north
-        ]
-        for dir in dirs:
-            if (0 <= (col + dir[0]) < self.width and
-                    0 <= (row + dir[1]) < self.height and
-                    self.visited[row + dir[1], col + dir[0]] == 0):
-                walls.append((col + dir[0], row + dir[1], dir[2], dir[3], col, row))
+        for dx, dy, wall in self._directions:
+            if (0 <= (col + dx) < self.width and
+                    0 <= (row + dy) < self.height and
+                    self.visited[row + dy, col + dx] == 0):
+                walls.append((col, row, wall))
         return walls
 
-    def remove_wall(self, neighbor: tuple):
-        ncol, nrow, wall, opposite, col, row = neighbor
-
-        self.maze[row, col] &= ~(1 << wall)
-        self.maze[nrow, ncol] &= ~(1 << opposite)
-
-    def write_42(self):
-        if self.height < 8 or self.width < 10:
-            return
-        pattern = [
-            '1   111',
-            '1     1',
-            '111 111',
-            '  1 1  ',
-            '  1 111'
-        ]
-        x0 = self.width // 2 - len(pattern[0]) // 2
-        y0 = self.height // 2 - len(pattern) // 2
-        for y, row in enumerate(pattern):
-            for x, value in enumerate(row):
-                if value != ' ':
-                    self.visited[y0 + y, x0 + x] = 1
-
-    def possible_walls (self, position: tuple[int, int]) -> list[int]:
-        col, row = position
-        num_position = self.maze[row, col]
-        walls = []
-        
-        for num in range(4):
-            is_outer_wall = (
-                (num == 0 and row == 0) or
-                (num == 1 and col == self.width - 1) or
-                (num == 2 and row == self.height - 1) or
-                (num == 3 and col == 0)
-            )
-            if (not is_outer_wall and num_position & (1 << num)):
-                walls.append(num)
-        return (walls)
-
-    def can_remove_wall(self, position: tuple[int, int], wall: int) -> bool:
-        col, row = position
-        dirs = [
-                    (0, -1),   # north
-                    (1, 0),   # east
-                    (0, 1),   # south
-                    (-1, 0)  # west            
-        ]
-        if self.maze[row][col] in [1, 2, 4, 8]:
-            return False
-        for sum_col in [1, -1]:
-            for sum_row in[1, -1]:
-                ncol = col + sum_col
-                nrow = row + sum_row
-            if 0 <= ncol < self.width and 0 <= nrow < self.height:
-                if self.maze[nrow][ncol] == 0:
-                    return False
-        self.maze[row, col] &= ~(1 << wall)
+    def remove_wall(self, neighbor: tuple) -> tuple[int, int]:
+        col, row, wall = neighbor 
         opposite = wall + 2
         if opposite > 3:
             opposite -= 4
-        self.maze[row + dirs[wall][1]][col + dirs[wall][0]] &= ~(1 << opposite)
-        return True
+        ncol = col + self._directions[wall][0]
+        nrow = row + self._directions[wall][1]
+        self.maze[row, col] &= ~(1 << wall)
+        self.maze[nrow, ncol] &= ~(1 << opposite)
+        return (ncol, nrow)
+
+    def possible_walls(self, position: tuple[int, int]) -> list[int]:
+        col, row = position
+        num_position = self.maze[row, col]
+        walls: list[int] = []
+        if not num_position == 15:
+            for num in range(4):
+                is_outer_wall = (
+                    (num == 0 and row == 0) or
+                    (num == 1 and col == self.width - 1) or
+                    (num == 2 and row == self.height - 1) or
+                    (num == 3 and col == 0)
+                )
+                if (not is_outer_wall and num_position & (1 << num)):
+                    cells_with_walls = True
+                    for sum_col in [1, 0, -1]:
+                        for sum_row in [1, 0, -1]:
+                            ncol = col + sum_col
+                            nrow = row + sum_row
+                            if 0 <= ncol < self.width and 0 <= nrow < self.height:
+                                if self.maze[nrow][ncol] == 0:
+                                    cells_with_walls = False
+                                    break
+                        if not cells_with_walls:
+                            break
+                    if cells_with_walls:
+                        walls.append(num)
+        return walls
 
     def make_no_perfect(self):
         num_cells = self.width * self.height
@@ -164,6 +168,7 @@ class MazeGenerator():
             cut_cells = math.ceil(0.15 * num_cells)
         else:
             cut_cells = 0.20 * num_cells
+
         solution = SolutionGenerator(self)
         solution.get_solution()
         sol = solution.solution
@@ -171,16 +176,12 @@ class MazeGenerator():
         while num_solutions <= 1:
             cells = 0
             while cells < cut_cells:
-                removed =  False
-                while removed == False:
-                    position = (random.randint(0, self.width -1), random.randint(0, self.height -1))
-                    walls = self.possible_walls(position)
-                    for wall in walls:
-                        removed  = self.can_remove_wall(position, wall)
-                        if removed:
-                            cells += 1
-                            walls.clear()
-                            break
+                position = (random.randint(0, self.width - 1), random.randint(0, self.height -1))
+                walls = self.possible_walls(position)
+                if walls:
+                    wall = random.choice(walls)
+                    self.remove_wall((position[0], position[1], wall))
+                    cells += 1
             solution = SolutionGenerator(self)
             solution.get_solution()
             sol = solution.solution
@@ -191,18 +192,17 @@ class MazeGenerator():
         stack = [(self.entry[0], self.entry[1])]
         while stack:
             col, row = stack[-1]
-            walls = self.get_neighbors(col, row)
+            walls = self.get_available_walls(col, row)
             if walls:
                 wall = random.choice(walls)
-                ncol, nrow = wall[0], wall[1]
-                self.remove_wall(wall)
+                ncol, nrow = self.remove_wall(wall)
                 self.visited[nrow, ncol] = 1
                 stack.append((ncol, nrow))
             else:
                 stack.pop()
         if not self.perfect:
+            print("no perfeec")
             self.make_no_perfect()
-
 
 
 class SolutionGenerator():
@@ -223,7 +223,7 @@ class SolutionGenerator():
         num_position = self.maze.maze[row, col]
         for num in range(4):
             if not (num_position & (1 << num)):
-                x, y, point  = dirs[num]
+                x, y, point = dirs[num]
                 nx, ny = col + x, row + y
                 neighbors.append((nx, ny, point))
         return neighbors
@@ -234,17 +234,15 @@ class SolutionGenerator():
         while stack:
             position, sol, visited = stack[0]
             if position == self.maze.entry:
-                self.solution.append ("".join(stack[0][1][::-1]))
+                self.solution.append("".join(stack[0][1][::-1]))
             else:
                 neighbours = self.get_neighbors(position[0], position[1])
                 for x, y, point in neighbours:
                     if (x, y) not in visited:
                         new_sol = sol + [point]
-                        new_visited = visited + [(x,y)]
+                        new_visited = visited + [(x, y)]
                         stack.append([(x, y), new_sol, new_visited])
             stack.pop(0)
-        
-
 
 
 def read_configuration(file_name: str) -> Configuration:
@@ -291,6 +289,7 @@ def main():
         file.write(f"{maze_1.exit[0]},{maze_1.exit[1]}\n")
         file.write(f"{solution.solution[0]}\n")
     
+    print(maze_1.maze)
     print(solution.solution)
 
 
